1e6de763b3e37e087a90e504280dfd3f126cd1b4
Nikita Petrenkov <nikita.petrenkov@rtlabs.ru>
2021-07-25 13:21:49 +0400

    Initial Commit

---
+++ TableView3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView3
+//
+//  Created by Nikita Petrenkov on 25.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
